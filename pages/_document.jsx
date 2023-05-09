35ee195810bea022a59537a7a8771e446cc4810c
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-05-09 21:09:12 +0200

    Netlify identity and get project concept

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script defer src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,20 @@
 import Layout from '@/components/layout/layout/layout';
+import { useNetlifyIdentityRedirectHook } from '@/lib/customHooks';
+import { getProjects } from '@/lib/utils';
 
-function Home() {
+export async function getStaticProps() {
+  const projects = await getProjects();
+
+  return {
+    props: {
+      projects,
+    },
+  };
+}
+
+function Home({ projects }) {
+  useNetlifyIdentityRedirectHook();
+
   return (
     <main className={`flex flex-col items-center justify-between p-24`}>
       <h1 className="text-4xl font-bold text-center">Welcome to my personal portfolio website!</h1>
