2c5599e731f9232f4180915aecb8842d505f0620
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-05-09 21:08:52 +0200

    Added mdContent

---
+++ components/mdContent/mdContent.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+import contentStyles from "./mdContent.module.scss"
+import CategoryLabel from "../categoryLabel/categoryLabel"
+import { useDetailTagsAnimation, useInstagramEmbedScript } from "../../lib/markdownEnrichment"
+import Link from "next/link"
+import StartEndDateLabel from "../layout/util/startEndDateLabel/startEndDateLabel";
+
+const MdContent = ({ content: { title, date, startDate, endDate, thumbnail, category, content }, noDate, categoriesLink }) => {
+
+  useDetailTagsAnimation()
+
+  useInstagramEmbedScript(content)
+
+  return (
+    <article className={contentStyles.content}>
+      <div className="flex justify-between items-center flex-wrap gap-2">
+        <h1 className={contentStyles.mainTitle}>{title}</h1>
+        {categoriesLink && <Link href="/blog/categories" className="text-text-primary h-100">All Categories</Link>}
+      </div>
+      <div className={contentStyles.metaData}>
+        {!noDate && date && <time className="m-0">{date}</time>}
+        {startDate && <StartEndDateLabel startDate={startDate} endDate={endDate}/>}
+        {category && <CategoryLabel category={category}/>}
+      </div>
+      {thumbnail && <img className="w-full" alt="thumbnail" src={thumbnail}/>}
+      <div className="relative z-[1]" dangerouslySetInnerHTML={{ __html: content }}/>
+    </article>
+  )
+}
+
+export default MdContent
