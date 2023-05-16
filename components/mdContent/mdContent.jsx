a7a5c95f5ee02dff7e6ead19322f4f3d42e2eae7
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-05-17 01:14:43 +0200

    Added mdContent.module.scss & mdContent.jsx

--- components/mdContent/mdContent.jsx
+++ components/mdContent/mdContent.jsx
@@ -1,31 +1,24 @@
-import React from "react"
-import contentStyles from "./mdContent.module.scss"
-import CategoryLabel from "../categoryLabel/categoryLabel"
-import { useDetailTagsAnimation, useInstagramEmbedScript } from "../../lib/markdownEnrichment"
-import Link from "next/link"
-import StartEndDateLabel from "../layout/util/startEndDateLabel/startEndDateLabel";
+import React from 'react';
+import contentStyles from './mdContent.module.scss';
+import StartEndDateLabel from '@/components/common/startEndDateLabel';
+import { useDetailTagsAnimation } from '@/lib/markdownEnrichment';
 
-const MdContent = ({ content: { title, date, startDate, endDate, thumbnail, category, content }, noDate, categoriesLink }) => {
-
-  useDetailTagsAnimation()
-
-  useInstagramEmbedScript(content)
+const MdContent = ({ content: { title, date, startDate, endDate, thumbnail, content }, noDate }) => {
+  useDetailTagsAnimation();
 
   return (
     <article className={contentStyles.content}>
       <div className="flex justify-between items-center flex-wrap gap-2">
         <h1 className={contentStyles.mainTitle}>{title}</h1>
-        {categoriesLink && <Link href="/blog/categories" className="text-text-primary h-100">All Categories</Link>}
       </div>
       <div className={contentStyles.metaData}>
         {!noDate && date && <time className="m-0">{date}</time>}
-        {startDate && <StartEndDateLabel startDate={startDate} endDate={endDate}/>}
-        {category && <CategoryLabel category={category}/>}
+        {startDate && <StartEndDateLabel startDate={startDate} endDate={endDate} />}
       </div>
-      {thumbnail && <img className="w-full" alt="thumbnail" src={thumbnail}/>}
-      <div className="relative z-[1]" dangerouslySetInnerHTML={{ __html: content }}/>
+      {thumbnail && <img className="w-full" alt="thumbnail" src={thumbnail} />}
+      <div className="relative z-[1]" dangerouslySetInnerHTML={{ __html: content }} />
     </article>
-  )
-}
+  );
+};
 
-export default MdContent
+export default MdContent;
