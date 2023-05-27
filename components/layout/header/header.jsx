ddf9105aa3f444e6454d0513f6c1d7a337273005
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-05-27 23:40:48 +0200

    Update header

--- components/layout/header/header.jsx
+++ components/layout/header/header.jsx
@@ -1,9 +1,13 @@
-import React from "react"
+import React from 'react';
 
 export default function Header({}) {
   return (
-    <header id="header" className="h-24 bg-amber-700">
-
+    <header id="header" className="h-16 px-4 flex items-center bg-amber-700">
+      <span className="text-3xl">
+        <h1 className="sm:hidden">J.W.</h1>
+        <h1 className="hidden sm:block md:hidden">J.W. van Bremen</h1>
+        <h1 className="hidden md:block">Jan-Willem van Bremen</h1>
+      </span>
     </header>
-    )
+  );
 }
