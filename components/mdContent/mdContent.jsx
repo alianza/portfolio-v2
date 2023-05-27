b2957cb5d3a0877ec718bd826c0fca8f588c0b2e
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-05-27 13:31:20 +0200

    Added useDarkMode custom hook and use in components

--- components/mdContent/mdContent.jsx
+++ components/mdContent/mdContent.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import contentStyles from './mdContent.module.scss';
 import StartEndDateLabel from '@/components/common/startEndDateLabel';
 import { useDetailTagsAnimation } from '@/lib/markdownEnrichment';
+import { useDarkMode } from '@/lib/customHooks';
+import Image from 'next/image';
 
 const MdContent = ({ content: { title, date, startDate, endDate, thumbnail, content }, noDate }) => {
+  const darkMode = useDarkMode();
+
   useDetailTagsAnimation();
+
+  useEffect(() => {
+    if (darkMode === undefined) return;
+    if (darkMode) {
+      import('highlight.js/styles/a11y-dark.css');
+    } else {
+      import('highlight.js/styles/a11y-light.css');
+    }
+  }, [darkMode]);
 
   return (
     <article className={contentStyles.content}>
@@ -15,7 +28,18 @@ const MdContent = ({ content: { title, date, startDate, endDate, thumbnail, cont
         {!noDate && date && <time className="m-0">{date}</time>}
         {startDate && <StartEndDateLabel startDate={startDate} endDate={endDate} />}
       </div>
-      {thumbnail && <img className="w-full" alt="thumbnail" src={thumbnail} />}
+      {thumbnail && (
+        <Image
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="w-full h-auto"
+          alt={`${title} thumbnail`}
+          src={thumbnail}
+          placeholder="blur"
+          blurDataURL={`/_next/image?url=${thumbnail}&w=16&q=1`}
+        />
+      )}
       <div className="relative z-[1]" dangerouslySetInnerHTML={{ __html: content }} />
     </article>
   );
