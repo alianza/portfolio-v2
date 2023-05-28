96d9a10198430b34f267cda43acd511664d6a37e
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-05-28 14:54:31 +0200

    Cleanup

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,7 +26,7 @@ function Home({ projects }) {
       <div className="w-full">
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-4/5">
           <h1 className="text-5xl font-bold drop-shadow-lg">Jan-Willem van Bremen</h1>
-          <h2 className="text-3xl drop-shadow-lg">Software developer, Skateboarder & Model!</h2>
+          <h2 className="text-3xl drop-shadow-lg">Software engineer, Skateboarder & Model!</h2>
         </div>
         <video
           className="object-cover pointer-events-none w-full h-[calc(100vh-theme(spacing.header))]"
