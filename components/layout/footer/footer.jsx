5f173a8e2e315160391964d2b4bc4f8fb3002cb9
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-05-29 02:00:59 +0200

    Added footer color

--- components/layout/footer/footer.jsx
+++ components/layout/footer/footer.jsx
@@ -1,9 +1,5 @@
-import React from "react"
+import React from 'react';
 
-export default function Footer({ }) {
-  return (
-    <footer id="footer" className="h-24 mt-auto bg-amber-500">
-
-    </footer>
-  )
+export default function Footer({}) {
+  return <footer id="footer" className="mt-auto h-24 bg-neutral-700"></footer>;
 }
