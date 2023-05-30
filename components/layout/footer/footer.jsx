a5c0dac0e268c70ffe4ebf43f2d353c049e53718
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-05-30 23:56:07 +0200

    Added footer content

--- components/layout/footer/footer.jsx
+++ components/layout/footer/footer.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import config from '../../../content/config.json';
+import utilStyles from '../../../styles/utils.module.scss';
 
 export default function Footer({}) {
-  return <footer id="footer" className="mt-auto h-24 bg-neutral-700"></footer>;
+  return (
+    <footer id="footer" className="mt-auto flex flex-wrap items-center justify-between bg-neutral-700 px-4">
+      <p className="my-2 font-semibold">Author: Jan-Willem van Bremen</p>
+      <div className="my-2 flex flex-wrap gap-4 sm:flex-nowrap">
+        {config.accounts.map(({ name, url, icon }) => (
+          <a className={`${utilStyles.link}`} key={name} href={url}>
+            {name}
+          </a>
+        ))}
+      </div>
+    </footer>
+  );
 }
