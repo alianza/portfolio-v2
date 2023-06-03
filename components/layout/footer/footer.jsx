37505c8bb3ca21c3e02eed329fbee2b3b9e2b1fe
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-06-03 15:36:56 +0200

    Set header text color

--- components/layout/footer/footer.jsx
+++ components/layout/footer/footer.jsx
@@ -4,7 +4,10 @@ import utilStyles from '../../../styles/utils.module.scss';
 
 export default function Footer({}) {
   return (
-    <footer id="footer" className="mt-auto flex flex-wrap items-center justify-between bg-neutral-700 px-4">
+    <footer
+      id="footer"
+      className="mt-auto flex flex-wrap items-center justify-between bg-neutral-700 px-4 text-neutral-50"
+    >
       <p className="my-2 font-semibold">Author: Jan-Willem van Bremen</p>
       <div className="my-2 flex flex-wrap gap-4 sm:flex-nowrap">
         {config.accounts.map(({ name, url, icon }) => (
