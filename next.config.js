f039c683060092b2d6399c28b4480d08824c72cd
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-02-06 15:35:24 +0100

    Added better logging

--- next.config.js
+++ next.config.js
@@ -10,13 +10,12 @@ const nextConfig = {
   const config = process.env;
 
   const yamlContent = fs.readFileSync('./public/admin/config.example.yml', 'utf8');
-  const interpolatedYamlContent = yamlContent.replace(
-    /\${(\w+):-(\w+)}/g,
-    (_, key, defaultValue) => config[key] || defaultValue,
-  );
+  const interpolatedYamlContent = yamlContent.replace(/\${(\w+):-(\w+)}/g, (_, key, defaultValue) => {
+    console.info('Replacing CMS config key:', key, 'with', config[key] || defaultValue);
+    return config[key] || defaultValue; // Replace ${key:-defaultValue} with the value of key from the config or defaultValue if key is not present
+  });
   const parsedConfig = yaml.load(interpolatedYamlContent);
   fs.writeFileSync('./public/admin/config.yml', yaml.dump(parsedConfig));
-  console.log(`parsedConfig`, parsedConfig);
 })();
 
 module.exports = nextConfig;
