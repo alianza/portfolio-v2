d2e43d5dff84df26cc7347bc6b826a783a8a68fb
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-02-06 15:20:23 +0100

    Added dynamic local cms setting through environment variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const DEFAULT_ENV = {
+  CMS_LOCAL_BACKEND: false,
+};
+
+(config = () => {
+  const config = process.env;
+
+  const yamlContent = fs.readFileSync('./public/admin/config.example.yml', 'utf8');
+  const interpolatedYamlContent = yamlContent.replace(
+    /\${(\w+)}/g,
+    (match, envVar) => config[envVar] || DEFAULT_ENV[envVar] || '',
+  );
+  const parsedConfig = yaml.load(interpolatedYamlContent);
+  fs.writeFileSync('./public/admin/config.yml', yaml.dump(parsedConfig));
+})();
+
+module.exports = nextConfig;
